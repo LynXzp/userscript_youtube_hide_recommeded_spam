f819442b2324a2102f995c3727ae7ce6faa58481
LynXzp <lynxzp@gmail.com>
2016-09-05 01:10:46 +0300

    Create youtube_hide_recommeded_spam.js

---
+++ youtube_hide_recommeded_spam.js
@@ -0,0 +1,22 @@
+// ==UserScript==
+// @name        youtube_hide_recommeded_spam
+// @namespace   body
+// @include     *youtube.com/*
+// @version     1
+// @grant       none
+// ==/UserScript==
+
+var timerId = setInterval(
+  function() {
+    var elems = document.getElementsByClassName('content-wrapper');
+
+    for(var i=0; i<elems.length; i++)
+      { 
+        var elems2 = elems[i].getElementsByClassName('stat view-count');
+        for(var j=0; j<elems2.length; j++)
+      
+        if( elems2[j].innerHTML == 'Recommended for you')
+          elems[i].style.display = 'none'
+      }
+  },
+5000);
